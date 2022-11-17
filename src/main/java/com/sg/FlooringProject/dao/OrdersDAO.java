b5faaa2e7c8de4ef8fe3a356e88819663aab36cb
Claude Marcelle Seide <claudemarcelle1@outlook.com>
2022-11-17 13:51:50 -0600

    "I've updated the DAO,DAOIMPL, and added product files and taxes

--- src/main/java/com/sg/FlooringProject/dao/OrdersDAO.java
+++ src/main/java/com/sg/FlooringProject/dao/OrdersDAO.java
@@ -2,9 +2,30 @@ package com.sg.FlooringProject.dao;
 
 import com.sg.FlooringProject.dto.Orders;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 public interface OrdersDAO {
 Orders addOrders(int OrderNumber, LocalDate orderDate);
+
+List<Orders> getAllOrders();
+List<Integer> getAllOrderNumber();
+
+Orders getOrders (int OrderNumber,LocalDate orderDate);
+
+Orders CreateNewOrder(String newCustomerName, String newOrderState,
+                      String productType, BigDecimal newOrderArea);
+
+void addNewOrderToFile(LocalDate newOrderDate, Orders newOrder);
+Orders createEditedOrder(Orders order, String newName,
+                            String newState, String newProductType,
+                            BigDecimal newArea);
+
+void writeEditOrder(LocalDate orderDate, Orders orderToEdit, Orders editedOrder);
+
+Orders removeOrder(int OrderNumber);
+void exportAllData();
+
 
 }
--- src/main/java/com/sg/FlooringProject/dao/OrdersDaoImpl.java
+++ src/main/java/com/sg/FlooringProject/dao/OrdersDaoImpl.java
@@ -2,11 +2,64 @@ package com.sg.FlooringProject.dao;
 
 import com.sg.FlooringProject.dto.Orders;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class OrdersDaoImpl implements OrdersDAO{
+
+    private Map<Integer, Orders> orders =new HashMap<>();
+    private final String DELIMITER = ",";
+
     @Override
     public Orders addOrders(int OrderNumber, LocalDate orderDate) {
         return null;
+    }
+
+    @Override
+    public List<Orders> getAllOrders() {
+        return null;
+    }
+
+    @Override
+    public List<Integer> getAllOrderNumber() {
+        return null;
+    }
+
+    @Override
+    public Orders getOrders(int OrderNumber, LocalDate orderDate) {
+        return null;
+    }
+
+    @Override
+    public Orders CreateNewOrder(String newCustomerName, String newOrderState, String productType, BigDecimal newOrderArea) {
+        return null;
+    }
+
+    @Override
+    public void addNewOrderToFile(LocalDate newOrderDate, Orders newOrder) {
+
+    }
+
+    @Override
+    public Orders createEditedOrder(Orders order, String newName, String newState, String newProductType, BigDecimal newArea) {
+        return null;
+    }
+
+    @Override
+    public void writeEditOrder(LocalDate orderDate, Orders orderToEdit, Orders editedOrder) {
+
+    }
+
+    @Override
+    public Orders removeOrder(int OrderNumber) {
+        return null;
+    }
+
+    @Override
+    public void exportAllData() {
+
     }
 }
